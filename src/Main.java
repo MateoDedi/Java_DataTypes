8098968cbeac66fc6658b61c91124734af728c66
Mateo Dedi <mateod@live.com>
2024-01-22 22:31:51 +0100

    first

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        byte myByte = 10;
+//        short myShort = 20;
+//        int myInt = 30;
+//        long myLong = 40;
+//        float myFloat = 50.0;
+//        double myDouble = 60.0;
+//        boolean myBoolean = true;
+//        char myChar = 'A';
+//
+//        System.out.println("byte value: " + myByte);
+//        System.out.println("short value: " + myShort);
+//        System.out.println("int value: " + myInt);
+//        System.out.println("long value: " + myLong);
+//        System.out.println("float value: " + myFloat);
+//        System.out.println("double value: " + myDouble);
+//        System.out.println("boolean value: " + myBoolean);
+//        System.out.println("char value: " + myChar);
+//    }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+        byte myByte = 10;
+        short myShort = 20;
+        int myInt = 30;
+        long myLong = 40L;
+        float myFloat = 50.0f;
+        double myDouble = 60.0;
+        boolean myBoolean = true;
+        char myChar = 'A';
+
+        System.out.println("Byte value: " + myByte);
+        System.out.println("Short value: " + myShort);
+        System.out.println("Integer value: " + myInt);
+        System.out.println("Long value: " + myLong);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Boolean value: " + myBoolean);
+        System.out.println("Character value: " + myChar);
+    }
+}
